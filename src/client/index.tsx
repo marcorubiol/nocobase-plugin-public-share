c0e642eabab4129f50a2c88aab7119f25c777489
marcorubiol <marcorubiol@gmail.com>
2025-09-08 16:31:59 +0200

    fix(client): follow official NocoBase pattern - extend Plugin from @nocobase/client

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,29 +1,9 @@
-function PublicShareClientPlugin(this: any, app: any) {
-  this.app = app;
+import { Plugin } from '@nocobase/client';
+
+export class PublicShareClientPlugin extends Plugin {
+  async load() {
+    console.log('[PublicShare] Client plugin loaded successfully.');
+  }
 }
 
-// Add static options for NocoBase PluginManager
-(PublicShareClientPlugin as any).options = {
-  name: '@nocobase/plugin-public-share',
-};
-
-PublicShareClientPlugin.prototype.afterAdd = function () {};
-
-PublicShareClientPlugin.prototype.beforeLoad = async function () {};
-
-PublicShareClientPlugin.prototype.load = async function () {
-  console.log('[PublicShare] Client plugin loaded successfully.');
-};
-
-PublicShareClientPlugin.prototype.afterEnable = async function () {};
-
-PublicShareClientPlugin.prototype.afterDisable = async function () {};
-
-PublicShareClientPlugin.prototype.remove = async function () {};
-
 export default PublicShareClientPlugin;
-
-// Add options to the exports object too
-export const options = {
-  name: '@nocobase/plugin-public-share',
-};
