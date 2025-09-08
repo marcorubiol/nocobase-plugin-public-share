bec58abc039438e4cd1878c0e42698e56d1fb1a9
marcorubiol <marcorubiol@gmail.com>
2025-09-08 07:34:32 +0200

    fix(client): export default from AMD bundle for RequireJS loader

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,11 +1,5 @@
-// Define a very small client plugin inline to avoid module system issues.
-function ClientPlugin() {
+export default function ClientPlugin() {
   // eslint-disable-next-line no-console
   console.log('[PublicShare] Client plugin registered');
   return null;
 }
-
-// Expose as global so NocoBase can discover default export in browser without module loader
-(function (g: any) {
-  g['@nocobase/plugin-public-share'] = { default: ClientPlugin };
-})(globalThis as any);
