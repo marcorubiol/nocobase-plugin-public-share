97f4bc765cd062e1a6116e211e8ef04a35f9a28a
marcorubiol <marcorubiol@gmail.com>
2025-09-08 10:36:35 +0200

    fix(client): use plain function constructor for maximum loader compatibility

--- src/client/plugin.ts
+++ src/client/plugin.ts
@@ -1,32 +1,19 @@
-/**
- * A self-contained client plugin class that is compatible with NocoBase's client loader.
- * It is constructible (can be called with `new`) and has the static `options` property
- * and lifecycle methods that the PluginManager expects.
- * It does not extend any base class to avoid build-time module resolution errors.
- */
-export default class PublicShareClientPlugin {
-  static options = {
-    name: '@nocobase/plugin-public-share',
-  };
-
-  app: any;
-
-  constructor(app: any) {
-    this.app = app;
-  }
-
-  afterAdd() {}
-
-  async beforeLoad() {}
-
-  async load() {
-    // eslint-disable-next-line no-console
-    console.log('[PublicShare] Client plugin loaded successfully.');
-  }
-
-  async afterEnable() {}
-
-  async afterDisable() {}
-
-  async remove() {}
+function PublicShareClientPlugin(this: any, app: any) {
+  this.app = app;
 }
+
+PublicShareClientPlugin.prototype.afterAdd = function () {};
+
+PublicShareClientPlugin.prototype.beforeLoad = async function () {};
+
+PublicShareClientPlugin.prototype.load = async function () {
+  console.log('[PublicShare] Client plugin loaded successfully.');
+};
+
+PublicShareClientPlugin.prototype.afterEnable = async function () {};
+
+PublicShareClientPlugin.prototype.afterDisable = async function () {};
+
+PublicShareClientPlugin.prototype.remove = async function () {};
+
+export default PublicShareClientPlugin;
